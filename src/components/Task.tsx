f9fa4b93a82bed423fd98b33f2fc461c05f3b4d6
felipemattosv <felipe.vanetti@gmail.com>
2023-01-10 09:30:42 -0300

    adiciona estilizacao parcial de TasksList e Task

---
+++ src/components/Task.tsx
@@ -0,0 +1,15 @@
+import { Checkbox } from "@chakra-ui/react"
+
+
+export function Task() {
+    return (
+        <Checkbox
+        bg="gray.700"
+        padding="3"
+        spacing='1rem'
+        color="gray.50"
+        >
+            Task 1
+        </Checkbox>
+    )
+}
